6b49e75b91e5dfcb75fafb9b00c630b095307c40
cagla <caglatunc03@gmail.com>
2023-07-19 00:38:53 +0300

    .

--- 01.Degiskenler/Program.cs
+++ 01.Degiskenler/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");// Methottur.
+
+
+//string, int, decimal, double, float, bool, DateTime, Object
+
+string name;// string'in atamasız (default) değeri null'dır. Burada bir değer tanımlamadıkm ve kodu ; sonlandırdık.Memory de null olarak tuulur.
+
+int age; // age'in default değeri 0'dır.
+bool isActive; // bool'un default değeri false'dir.
+
+string nameLastName = " Çağla Tunç";
+Console.WriteLine(nameLastName);
+
+
+
+
+Console.ReadLine();
