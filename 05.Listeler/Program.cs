1dd2ae4252b0e7f10245a8ffc05123bdd8c44324
cagla <caglatunc03@gmail.com>
2023-07-19 05:50:15 +0300

    .

---
+++ 05.Listeler/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello, World!");
+
+//[ ] => Listeleri temsil eder => array işareti
+
+//string[] names = { "afsgag", "hshs" };
+string[] names = new string[3]; //içerisine kaç değer atacağımızı belirtmek gerekiyor
+names[0] = "Çağla";
+names[1] = "Çağla";
+names[2] = "Çağla";
+
+List<string> names = new (); // örneğe dönüştürmek için new kelimesini kullanıyoruz. Örneğe dönüşürme işleminde yazılımda "instance" üretme deniyor.OOP'de sınıfın nesneye dönüştürme manasına gelir.
+
+names.Add("Çağla");
+names.Add("Çağla");
+names.Add("Çağla");
+names.Add("Çağla");
+names.Add("Çağla");
+
+//class User
+//{
+//    public string Name;
+//}
+//var user1 = new User();
+//var user2 = new User();
+//var user3 = new User();
+//List<User> users = new List<User>();
+//List<User> users = new();
---
+++ 06.Donguler/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _06.Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> names = new();
+            names.Add("Taner");//0
+            names.Add("Tugay");//1
+            names.Add("Tahir");//2
+            names.Add("Toprak");//3
+            names.Add("Türkan");//4
+            names.Add("Seval");//5
+
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+            //Console.WriteLine(names[4]);
+            //Console.WriteLine(names[5]);
+
+            //for, foreach, while
+
+
+            //for (int x = 0; x < names.Count(); x++)
+            //{
+            //    Console.WriteLine(names[x]);
+            //}
+
+            //foreach var x in names)  //liste kadar dönderir.
+            //{
+            //Console.WriteLine(x);
+            //}
+            //int x = 0;
+            while(names.Count > 0) //while Sonsuz döngü oluşturur ya da şartlı hali var.
+            {
+               // x++;
+                //if (x == 5) break;
+
+               Console.WriteLine("Çalışıyor!");
+
+                for (int x = 0; x < names.Count(); x++)
+                {
+                    if(x==4)
+                    {
+                        break;//bu döngüyü sonlandırır.
+                    }
+                    if(x==2)
+                    {
+                        continue;//bu da döngüde kalan kısmı atar.
+                    }
+                }
+            }
+        }
+    }
+}
